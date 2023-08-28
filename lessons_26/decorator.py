cf58cf368a34ed45e3bc5f72a080af7aa7c8077d
TrMaksim <tracukmaksim@gmail.com>
2023-08-28 16:52:18 +0300

    Decorator Done

--- lessons_26/decorator.py
+++ lessons_26/decorator.py
@@ -2,51 +2,42 @@ import asyncio
 import time
 
 
-def sync_decorator(func):
+def async_sync_decorator(func):
     def wrapper(*args, **kwargs):
+        is_async = asyncio.iscoroutinefunction(func)
         start_time = time.time()
-        value = func(*args, **kwargs)
+        if is_async:
+            result = asyncio.run(func(*args, **kwargs))
+        else:
+            result = func(*args, **kwargs)
         end_time = time.time()
         execution_time = end_time - start_time
+        function_type = "Async" if is_async else "Sync"
         print(
-            f"Sync function {func.__name__} was executed in {execution_time:.6f} seconds"
-        )
-        return value
-
-    return wrapper
-
-
-def async_decorator(func):
-    async def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = await func(*args, **kwargs)
-        end_time = time.time()
-        execution_time = end_time - start_time
-        print(
-            f"Async function {func.__name__} was executed in {execution_time:.6f} seconds"
+            f"{function_type} function {func.__name__} was executed in {execution_time:.6f} seconds"
         )
         return result
 
     return wrapper
 
 
-@sync_decorator
+@async_sync_decorator
 def synchronous_factorial(n):
     if n == 1:
         return 1
     return synchronous_factorial(n - 1) * n
 
 
-@async_decorator
+@async_sync_decorator
 async def asynchronous_function():
     await asyncio.sleep(2)
     return "Async function executed"
 
 
-async def main():
+def main():
     synchronous_factorial(5)
-    await asynchronous_function()
+    asynchronous_function()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
