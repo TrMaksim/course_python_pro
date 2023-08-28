4741c8e617902aa92b99b5a0838ab4c19a58527c
TrMaksim <tracukmaksim@gmail.com>
2023-08-28 11:44:48 +0300

    Refactor Hometask

--- lessons_26/decorator.py
+++ lessons_26/decorator.py
@@ -1,28 +1,52 @@
+import asyncio
 import time
 
 
-def decorator(func):
+def sync_decorator(func):
     def wrapper(*args, **kwargs):
         start_time = time.time()
         value = func(*args, **kwargs)
         end_time = time.time()
         execution_time = end_time - start_time
-        print(f"Function {func.__name__} was executed in {execution_time:.6f} seconds")
+        print(
+            f"Sync function {func.__name__} was executed in {execution_time:.6f} seconds"
+        )
         return value
 
     return wrapper
 
 
-@decorator
-def factorial(n):
+def async_decorator(func):
+    async def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = await func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(
+            f"Async function {func.__name__} was executed in {execution_time:.6f} seconds"
+        )
+        return result
+
+    return wrapper
+
+
+@sync_decorator
+def synchronous_factorial(n):
     if n == 1:
         return 1
-    return factorial(n - 1) * n
+    return synchronous_factorial(n - 1) * n
 
 
-def main():
-    factorial(13)
+@async_decorator
+async def asynchronous_function():
+    await asyncio.sleep(2)
+    return "Async function executed"
+
+
+async def main():
+    synchronous_factorial(5)
+    await asynchronous_function()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
